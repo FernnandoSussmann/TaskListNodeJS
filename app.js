2e03883f415b8975bc3715c3fff2311105a3da0f
Fernnando Sussmann <fernnandoh@hotmail.com>
2019-02-17 12:49:07 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require("http");
+var port = 8081;
+
+http.createServer(requestListener).listen(port);
+console.log("Sever is listening on port:", port);
+
+function requestListener(req,res){
+    res.writeHead(200, {'Contenct-Type': 'text/plain'});
+    console.log("Request received, responding now ...");
+    res.end("Hello");
+}
