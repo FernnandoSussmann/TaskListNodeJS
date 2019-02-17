8f2b2b39144a4eb92eff2447c2829dfff07e80d9
Fernnando Sussmann <fernnandoh@hotmail.com>
2019-02-17 14:44:39 -0300

    Delete method

--- expressApp.js
+++ expressApp.js
@@ -10,15 +10,23 @@ app.use(bodyParser.json());
 var taskList = [];
 app.get('/list', function(req, res){
     res.send(taskList);
-})
+});
 
 app.post('/task', function(req,res){
     taskList.push(req.body.task);
     res.send(taskList);
-})
+});
 
 app.put('/task/:task_index', function(req, res){
     var taskIndex = req.params.task_index;
     taskList[taskIndex] = req.body.task
     res.send(taskList);
 });
+
+app.delete('/task/:task_index', function(req, res){
+    var taskIndex = req.params.task_index;
+    taskList.splice(taskIndex, 1)
+    res.send(taskList);
+});
+
+
